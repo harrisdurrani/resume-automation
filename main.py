dea4ca2cdfff7461cc15214ced3331da78e1b1bf
harrisdurrani <harris.durrani@hotmail.com>
2021-09-26 15:45:50 -0700

    read from path function added

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('This is where the code for the project goes')
+import os
+from dataclasses import dataclass
+
+@dataclass
+class Automate:
+
+    path: str =  r'D:\PY Files\My Projects\resume-automation\Target'
+
+
+    def get_path(self):
+        files = os.listdir(self.path)
+
+        for file in files:
+            print(file)
+
+if __name__ == "__main__":
+    a = Automate()
+
+    a.get_path()
