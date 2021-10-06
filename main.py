755f757da2aaef174541d4ffd8e1b327db934143
Harris <harris.durrani@hotmail.com>
2021-10-05 17:32:41 -0700

    added feature to create new directory

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from pathlib import Path
 @dataclass
 class Automate:
 
-    path: str = Path.cwd() / "Target"
+    file_path: str = Path.cwd() / "Target"
 
-    def read_files(self):
-        files = os.listdir(self.path)
+    def read_files(self,target=r"C:\Users\harri\Documents\Resume"):
+        files = os.listdir(self.file_path)
 
         for file in files:
             temp = file.split("_")
-            print(temp)
+            company_name = temp[1].strip(".txt")
+            try:
+                new_path = os.path.join(target, company_name)
+                os.mkdir(new_path)
+            except FileExistsError:
+                print("folder already exists")
 
+        print(target)
+
+                
 if __name__ == "__main__":
     a = Automate()
 
