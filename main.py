94f597785594c9098d0027b66b38626bb62ba58d
harrisdurrani <harris.durrani@hotmail.com>
2023-12-28 11:12:06 -0800

    script now fully creates new directory

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import os
 from dataclasses import dataclass
 from pathlib import Path
+import shutil
 
-@dataclass
-class Automate:
+def read_files():
+    path = r""
+    target = r""
+    dir_list = os.listdir(path)
+    for file in dir_list:
+        temp = file.split("_")
+        company_name = temp[1].strip(".txt")
 
-    file_path: str = Path.cwd() / "Target"
+        new_directory = os.path.join(target, company_name)
+        print(new_directory)
+        print(os.path.join(path, file))
+        print(file)
+        try:
+            os.mkdir(new_directory)
 
-    def read_files(self,target=r"C:\Users\harri\Documents\Resume"):
-        files = os.listdir(self.file_path)
+        except FileExistsError:
+            print("folder already exists")
+        if file.endswith(".txt"):
+            shutil.copy2(os.path.join(path, file), new_directory)
 
-        for file in files:
-            temp = file.split("_")
-            company_name = temp[1].strip(".txt")
-            try:
-                new_path = os.path.join(target, company_name)
-                os.mkdir(new_path)
-            except FileExistsError:
-                print("folder already exists")
+      
 
-        print(target)
-
-                
 if __name__ == "__main__":
-    a = Automate()
+    read_files()
+    # a = Automate()
 
-    a.read_files()
+    # a.read_files()
